65a3b2c4e93985eecf4723c470a2eafa8eb681f4
Ryan-McD <72583999+Ryan-McD@users.noreply.github.com>
2023-07-14 22:14:46 -0400

    Add outputs to sign csr

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,14 @@ output "csr" {
   value = vault_pki_secret_backend_intermediate_cert_request.pki_int.csr
 }
 
+output "int_pki_backend_path" {
+    value = vault_mount.intermediate.path
+}
+
+output "int_pki_client_role_name" {
+    value = vault_pki_secret_backend_role.client-certs.name
+}
+
 output client_cert {
-    value = vault_pki_secret_backend_cert.client-cert
+  value = vault_pki_secret_backend_cert.client-cert
 }
