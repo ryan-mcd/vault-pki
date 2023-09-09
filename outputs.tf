f3da22b303045252e3ce00667c046a4ae95c5c92
Ryan-McD <72583999+Ryan-McD@users.noreply.github.com>
2023-09-09 15:06:11 -0400

    feat: Add ca-certs as output

--- outputs.tf
+++ outputs.tf
@@ -10,6 +10,21 @@ output "int_pki_client_role_name" {
     value = vault_pki_secret_backend_role.client-certs.name
 }
 
-output client_cert {
+output "client_cert" {
   value = vault_pki_secret_backend_cert.client-cert
+}
+
+output "root_ca_cert" {
+  value = vault_pki_secret_backend_root_cert.root.certificate
+}
+
+output "int_ca_cert" {
+  value = vault_pki_secret_backend_intermediate_set_signed.pki_int.certificate
+}
+
+output "ca_cert" {
+  value = <<-EOT
+  ${vault_pki_secret_backend_root_cert.root.certificate}
+  ${vault_pki_secret_backend_intermediate_set_signed.pki_int.certificate}
+  EOT
 }
